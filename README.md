a6107d3d73bc4372c456feff40459013a0e478e1
Szieberth Ádám <sziebadam@gmail.com>
2021-08-01 17:34:30 +0200

    version 0.1.0 (open beta)

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 jsonfile
 ========
 
-version 0.0.9
+version 0.1.0
 
 The ``jsonfile`` Python module can be used to sync a JSON file on a disk with the corresponding Python object instance.
 
 Can be used to autosave JSON compatible Python data.
 
+By default the content of the JSON file is human readable and version control friendly.
+
+Please note that speed is not a priority. If you want a fast backend, use a database like SQLite or PostgreSQL.
+If a transaction contains many changes, I recommend you to turn off autosaving, do the changes, save(), and finally turn autosave back on.
+
+*As of version 0.1.0 and 2021-08-01, the ``jsonfile`` module is considered as open beta. I made a massive zen of Python rewrite. The API is more or less final. The 1.0 release version will be declared once there will be no bugs reported for a year. 1.0 will be published on PyPI with certainly more docstrings added.*
+
 ```python
 >>> import jsonfile
->>> dump_kwargs = dict(ensure_ascii=False, indent="\t", sort_keys=True)
->>> a = jsonfile.jsonfile("a.json", autosave=True, dump_kwargs=dump_kwargs)
+>>> a = jsonfile.jsonfile("a.json")
 >>> a.data
 Ellipsis
 >>> # Ellipsis indicates no data
@@ -27,16 +33,29 @@ Traceback (most recent call last):
 NameError: name 'a' is not defined
 >>> # lets reload the file!
 >>>
->>> a = jsonfile.jsonfile("a.json", autosave=True, dump_kwargs=dump_kwargs)
+>>> a = jsonfile.jsonfile("a.json")
 >>> a.data
 ['Hello', {'World': '!'}, 1234]
 >>> a.data.insert(1, "beautiful")
+>>> # naturally, also saved
 >>> a.data
 ['Hello', 'beautiful', {'World': '!'}, 1234]
->>> # naturally, also saved
-...
 >>> a.data = {"Hello":"World!"}
 >>> # and saved again...
-...
 >>> a.delete()
+>>> # file is deleted but the `a` object still persist
+>>> a.save()
+>>> # a.json file is recreated;
+>>> # note that you can turn on autosaving with `a.autosave=False`
+>>> # to avoid excessive disk usage and do explicit saves with this call
+>>> o = a.data.copy()
+>>> o
+{'Hello': 'World!'}
+>>> # this is a native Python representation of the internal data;
+>>> # changes made on it will not effect the content of the file
+>>> o["foo"] = "bar"
+>>> o
+{'Hello': 'World!', 'foo': 'bar'}
+>>> a.data = o
+>>> # now however, the file content is updated with it and saved
 ```
--- jsonfile.py
+++ jsonfile.py
@@ -1,10 +1,14 @@
 """
-The ``jsonfile`` Python module can be used to sync a JSON file
-on a disk with the corresponding Python object instance.
+The ``jsonfile`` Python module can be used to sync a JSON file on a disk with the
+corresponding Python object instance.
 
 Can be used to autosave JSON compatible Python data.
+
+By default the content of the JSON file is human readable and version control
+friendly.
 """
-__version__ = "0.0.9"
+
+__version__ = "0.1.0"
 
 
 
@@ -18,293 +22,405 @@ import tempfile
 
 class JSONFileBase:
 
-  @property
-  def root(self):
-    return self._root
+    @property
+    def jsonfile(self):
+        return self._jsonfile
 
-  def _get_adapter_or_value(self, data):
-    if isinstance(data, collections.abc.Mapping):
-      return JSONFileObject(self._root, data)
-    elif isinstance(data, str):
-      return data
-    elif isinstance(data, collections.abc.Sequence):
-      return JSONFileArray(self._root, data)
-    else:
-      return data
+    @staticmethod
+    def _inputvalue(value):
+        if isinstance(value, (collections.abc.Mapping, JSONFileObject)):
+            return {str(k): JSONFileBase._inputvalue(v) for k, v in value.items()}  # ensure string keys
+        elif isinstance(value, str):
+            return value
+        elif isinstance(value, (collections.abc.Sequence, JSONFileArray)):
+            return [JSONFileBase._inputvalue(v) for v in value]
+        else:
+            return value
 
-  @staticmethod
-  def _value_norm(value):
-    if isinstance(value, (
-        collections.abc.Mapping,
-        JSONFileObject,
-    )):
-      return {str(k): v for k, v in value.items()}
-          # JSON obejct keys must be strings
-    elif isinstance(value, str):
-      return value
-    elif isinstance(value, (
-        collections.abc.Sequence,
-        JSONFileArray,
-    )):
-      return list(value)
-    else:
-      return value
+    def _outputvalue(self, data):
+        if isinstance(data, str):  # string is a special sequence
+            return data
+        elif isinstance(data, collections.abc.Mapping):
+            return JSONFileObject(self._jsonfile, data)
+        elif isinstance(data, collections.abc.Sequence):
+            return JSONFileArray(self._jsonfile, data)
+        else:
+            return data
 
 
 
-class JSONFile(JSONFileBase):
+class JSONFileRoot(JSONFileBase):
 
-  def __init__(self, filepath, *,
-      data = ...,
-      default_data = ...,
-      autosave = False,
-      dump_kwargs = None,
-      load_kwargs = None,
-  ):
-    self._root = self
-    self.changed = None
-    self.default_data = default_data
-    self._data = (default_data if data is ... else data)
-    self.autosave = autosave
-    self.dump_kwargs = dump_kwargs or dict(
-        skipkeys=False,
-        ensure_ascii=True,
-        check_circular=True,
-        allow_nan=True,
-        cls=None,
-        indent=None,
-        separators=None,
-        default=None,
-        sort_keys=False,
-    )
-    self.load_kwargs = load_kwargs or dict(
-        cls=None,
-        object_hook=None,
-        parse_float=None,
-        parse_int=None,
-        parse_constant=None,
-        object_pairs_hook=None,
-    )
-    self.filepath = filepath
-    if self._data == self.default_data:
-      self.reload()
-    elif autosave:
-      self.save()
-    # print(self.filepath)
+    # As the main purpose of this module to provide a human readable and version control friendly dynamic data
+    # storage, I decided to diverge a little from the default JSON dump parameters.
+    default_dump_kwargs = {
+        "skipkeys": False,
+        "ensure_ascii": False,  # +human readable (UTF)
+        "check_circular": True,
+        "allow_nan": True,
+        "cls": None,
+        "indent": "\t",  # +human readable
+        "separators": None,
+        "default": None,
+        "sort_keys": True,  # +version control friendliness
+    }
 
-  def __repr__(self):
-    return f"{self.__class__.__name__}('{self.filepath}')"
+    default_load_kwargs = {
+        "cls": None,
+        "object_hook": None,
+        "parse_float": None,
+        "parse_int": None,
+        "parse_constant": None,
+        "object_pairs_hook": None,
+    }
 
-  def __str__(self):
-    return self.__repr__()
+    def __init__(self, path, *, data=..., autosave=True, dump_kwargs=None, load_kwargs=None):
+        self._jsonfile = self
+        self._data = data
+        self.autosave = autosave
+        self.dump_kwargs = self.default_dump_kwargs if dump_kwargs is None else dump_kwargs
+        self.load_kwargs = self.default_load_kwargs if load_kwargs is None else load_kwargs
+        self.path = path
+        if data is ...:
+            if self.path.is_file():
+                self.load()
+        else:
+            if autosave:
+                self.save()
 
-  @property
-  def autosave(self):
-    return bool(self._autosave)  # ensure boolean
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.path}')"
 
-  @autosave.setter
-  def autosave(self, value):
-    self._autosave = bool(value)  # ensure boolean
+    def __str__(self):
+        return self.__repr__()
 
-  @property
-  def data(self):
-    return self._get_adapter_or_value(self._data)
+    def copy(self, path, *, autosave=..., dump_kwargs=..., load_kwargs=...):
+        path = pathlib.Path(path)
+        assert path != self.path
+        autosave = autosave if autosave is not ... else self.autosave
+        dump_kwargs = dump_kwargs if dump_kwargs is not ... else self.dump_kwargs
+        load_kwargs = load_kwargs if load_kwargs is not ... else self.load_kwargs
+        return JSONFileRoot(
+            path,
+            data=self._data,
+            autosave=autosave,
+            dump_kwargs=dump_kwargs,
+            load_kwargs=load_kwargs,
+        )
 
-  @data.setter
-  def data(self, value):
-    if value is ...:
-      raise ValueError("Ellipsis is forbidden, call delete()")
-    old_data = copy.copy(self._data)
-    self._data = self._value_norm(value)
-    self.may_changed(self, old_data)
+    @property
+    def data(self):
+        return self._outputvalue(self._data)
+    @data.setter
+    def data(self, value):
+        self._data = self._inputvalue(value)
+        if self.autosave:
+            self.save()
 
-  @property
-  def filepath(self):
-    return self._filepath
+    def delete(self):
+        self.path.unlink()
 
-  @filepath.setter
-  def filepath(self, value):
-    self._filepath = pathlib.Path(value) # ensure Path instance
+    def load(self):
+        p = self.path
+        path_exists = p.is_file()
+        path_is_not_empty = bool(p.stat().st_size)
+        if path_exists and path_is_not_empty:
+            with p.open(encoding="utf8") as f:
+                self._data = json.load(f, **self.load_kwargs)
+        else:
+            self._data = ...
 
-  def delete(self):
-    # could have been @data.deleter but that would be obscure
-    old_data = copy.copy(self._data)
-    self._data = self.default_data
-    self.may_changed(self, old_data)
-    try:
-      self.filepath.unlink()
-    except FileNotFoundError:
-      pass  # delete can happen previously
+    @property
+    def path(self):
+        return self._path
+    @path.setter
+    def path(self, value):
+        self._path = pathlib.Path(value) # ensure Path instance
 
-  def may_changed(self, inst, old_data):
-    if inst._data != old_data:
-      self.changed = True
-      self.on_change()
-
-  def on_change(self):
-    #print("on change", self, self.autosave)
-    if self.autosave:
-      self.save()
-
-  def reload(self):
-    p = self.filepath
-    if (
-        p.is_file()  # filepath_exists
-        and p.stat().st_size  # filepath_not_empty
-    ):
-      with p.open(encoding="utf8") as f:
-        self._data = json.load(f, **self.load_kwargs)
-        self.changed = False
-    else:
-        self._data = copy.deepcopy(self.default_data)
-        self.changed = None
-
-  def save(self, ensure_parents=True):
-    p = self.filepath
-    if self._data == self.default_data and p.is_file():
-      p.unlink()
-    else:
-      s = json.dumps(self._data, **self.dump_kwargs)
-      if ensure_parents:  # ensure parent directories
-        p.parent.mkdir(parents=True, exist_ok=True)
-      with tempfile.NamedTemporaryFile(
-          'w',
-          dir = p.parent,
-          delete = False,
-          encoding="utf8",
-      ) as tf:
-        tf.write(s)
-        tp = p.parent / tf.name
-      tp.replace(p)
+    def save(self, *, ensure_parents=True):
+        p = self.path
+        s = json.dumps(self._data, **self.dump_kwargs)
+        if ensure_parents:  # ensure parent directories
+            p.parent.mkdir(parents=True, exist_ok=True)
+        with tempfile.NamedTemporaryFile("w", dir=p.parent, delete=False, encoding="utf8") as tf:
+            tf.write(s)
+            tp = p.parent / tf.name
+        tp.replace(p)
 
 
 
 class JSONFileContainer(JSONFileBase):
 
-  def __init__(self, root, data):
-    self._root = root
-    self._data = data
+    _methods_which_cause_change = ()
 
-  def __contains__(self, key):  # has to be explicit
-    return self._data.__contains__(key)
+    def __init__(self, jsonfile, data):
+        self._jsonfile = jsonfile
+        self._data = data
 
-  def __delitem__(self, key):  # has to be explicit
-    m = self._change_method("__delitem__")
-    return m(key)
+    def __contains__(self, key):
+        return self._data.__contains__(key)
 
-  def __eq__(self, other):  # has to be explicit
-    return self._data.__eq__(other)
+    def __delitem__(self, key):
+        self._data.__delitem__(key)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
 
-  def __dir__(self):
-    return self._data.__dir__()
+    def __dir__(self):
+        return sorted(self._data.__dir__() + ["jsonfile"])
 
-  @property
-  def __doc__(self):
-    return self._data.__doc__
+    @property
+    def __doc__(self):
+        return self._data.__doc__
 
-  def __iter__(self):  # has to be explicit
-    return self._data.__iter__()
+    def __eq__(self, other):
+        return self._data.__eq__(other if not hasattr(other, "_data") else other._data)
 
-  def __getattr__(self, name):
-    return getattr(self._data, name)
+    def __getattr__(self, name):
+        return getattr(self._data, name)
 
-  def __getattribute__(self, name):
-    Ns = object.__getattribute__(self, "_change_method_names")
-    if name in Ns:
-      m = object.__getattribute__(self, "_change_method")
-      return m(name)
-    return object.__getattribute__(self, name)
+    def __getitem__(self, key):
+        data = self._data[key]
+        return self._outputvalue(data)
 
-  def __getitem__(self, key):
-    data = self._data[key]
-    return self._get_adapter_or_value(data)
+    def __len__(self):
+        return self._data.__len__()
 
-  def __len__(self):
-    return self._data.__len__()
+    def __ne__(self, other):
+        return self._data.__ne__(other if not hasattr(other, "_data") else other._data)
 
-  def __ne__(self, other):  # has to be explicit
-    return self._data.__ne__(other)
+    def __repr__(self):
+        return self._data.__repr__()
 
-  def __repr__(self):
-    return self._data.__repr__()
+    def __setitem__(self, key, value):
+        self._data.__setitem__(key, value)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
 
-  def __setitem__(self, key, value):  # has to be explicit
-    m = self._change_method("__setitem__")
-    return m(key, self._value_norm(value))
+    def __sizeof__(self):
+        # TODO(adam): not sure about this
+        return object.__sizeof__(self) + self._data.__sizeof__()
 
-  def _change_method(self, name):
-    def wrapped_method(*args, **kwargs):
-      old_data = copy.copy(self._data)
-      _data = object.__getattribute__(self, "_data")
-      m = getattr(_data, name)
-      try:
-        r = m(*args, **kwargs)
-        self._root.may_changed(self, old_data)
-      except:
-        self._data = old_data
-        raise
-      return r
-    return wrapped_method
+    def clear(self):
+        self._data.clear()
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def copy(self):
+        """a deep copy of the internal data"""
+        return copy.deepcopy(self._data)
 
 
 
 class JSONFileArray(JSONFileContainer, list):
 
-  _change_method_names = (
-    "__delitem__",
-    "__iadd__",
-    "__imul__",
-    "__setitem__",
-    "append",
-    "clear",
-    "extend",
-    "insert",
-    "pop",
-    "remove",
-    "reverse",
-    "sort",
-  )
+    def __add__(self, value):
+        raise AttributeError(
+            f"type object {self.__class__.__name__!r} has no attribute '__add__'"
+            f"\n{self.__class__.__name__!r} instances are not meant to be created directly"
+            "\nHint: you can add in place with the `+=` operator"
+            " or you can manipulate the copy of the internal data"
+        )
+
+    def __dir__(self):
+        names = set(super().__dir__())
+        names.remove("__add__")  # JSONFileObject instances are not meant to be created directly
+        names.remove("__mul__")  # same reason
+        names.remove("__rmul__")  # same reason
+        return sorted(names)
+
+    def __ge__(self, other):
+        return self._data.__ge__(other if not hasattr(other, "_data") else other._data)
+
+    def __gt__(self, other):
+        return self._data.__gt__(other if not hasattr(other, "_data") else other._data)
+
+    def __iadd__(self, other):
+        self._data += self._inputvalue(other)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def __imul__(self, other):
+        self._data *= self._inputvalue(other)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def __iter__(self):
+        return JSONFileArray_iterator(self)
+
+    def __le__(self, other):
+        return self._data.__le__(other if not hasattr(other, "_data") else other._data)
+
+    def __lt__(self, other):
+        return self._data.__lt__(other if not hasattr(other, "_data") else other._data)
+
+    def __mul__(self, value):
+        raise AttributeError(
+            f"type object {self.__class__.__name__!r} has no attribute '__mul__'"
+            f"\n{self.__class__.__name__!r} instances are not meant to be created directly"
+            "\nHint: you can multiply in place with the `*=` operator"
+            " or you can manipulate the copy of the internal data"
+        )
+
+    def __reversed__(self):
+        return JSONFileArray_reverseiterator(self)
+
+    def __rmul__(self, value):
+        raise AttributeError(
+            f"type object {self.__class__.__name__!r} has no attribute '__rmul__'"
+            f"\n{self.__class__.__name__!r} instances are not meant to be created directly"
+            "\nHint: you can multiply in place with the `*=` operator"
+            " or you can manipulate the copy of the internal data"
+        )
+
+    def append(self, item):
+        self._data.append(self._inputvalue(item))
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def count(self, value):
+        return self._data.count(value)
+
+    def extend(self, iterable):
+        self._data.extend(self._inputvalue(x) for x in iterable)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def index(self, value, start=0, stop=9223372036854775807):
+        return self._data.index(value, start, stop)
+
+    def insert(self, i, elem):
+        self._data.insert(i, self._inputvalue(elem))
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def pop(self, index=-1):
+        returnobj = self._data.pop(index)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+        return returnobj
+
+    def remove(self, value):
+        self._data.remove(value)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def reverse(self):
+        self._data.reverse()
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def sort(self):
+        self._data.sort()
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+
+
+class JSONFileArray_iterator():
+
+    def __init__(self, obj):
+        self._obj = obj
+        self._i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._i == len(self._obj):
+            raise StopIteration
+        returnobj = self._obj[self._i]
+        self._i += 1
+        return returnobj
+
+
+
+class JSONFileArray_reverseiterator():
+
+    def __init__(self, obj):
+        self._obj = obj
+        self._i = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self._obj) < -self._i:
+            raise StopIteration
+        returnobj = self._obj[self._i]
+        self._i -= 1
+        return returnobj
 
 
 
 class JSONFileObject(JSONFileContainer, dict):
 
-  _change_method_names = (
-    "__delitem__",
-    "__setitem__",
-    "clear",
-    "pop",
-    "popitem",
-    # "setdefault",  # handled exceptionally
-    "update",
-  )
+    def __dir__(self):
+        names = set(super().__dir__())
+        names.remove("fromkeys")  # JSONFileObject instances are not meant to be created directly
+        return sorted(names)
 
-  def __setitem__(self, key, value):  # has to be explicit
-    key = str(key)  # JSON obejct keys must be strings
-    return super().__setitem__(key, value)
+    def __iter__(self):
+        # no need of custom iterator as dict.__iter__ only return the keys
+        return self._data.__iter__()
 
-  def get(self, key, default=None):
-    # avoid return of list/dict objects
-    try:
-      return self[key]
-    except KeyError:
-      return default
+    def __reversed__(self):
+        # no need of custom iterator as dict.__reversed__ only return the keys
+        return self._data.__reversed__()
 
-  def items(self):
-    for k in self:
-      yield k, self[k]
+    def __setitem__(self, key, value):
+        key = str(key)  # ensure that JSON object keys are strings
+        return super().__setitem__(key, self._inputvalue(value))
 
-  def setdefault(self, key, default):
-    try:
-      return self[key]
-    except KeyError:
-      self[key] = default
-    return self[key]
+    @classmethod
+    def fromkeys(cls, iterable, value=None):
+        raise AttributeError(
+            f"type object {cls.__name__!r} has no attribute 'fromkeys'"
+            f"\n{cls.__name__!r} instances are not meant to be created directly"
+        )
 
-  def values(self):
-    for k in self:
-      yield self[k]
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def items(self):
+        for k in self:
+            yield k, self[k]
+
+    def keys(self):
+        return self._data.keys()
+
+    def pop(self, key, default):
+        returnobj = self._data.pop(key, default)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+        return returnobj
+
+    def popitem(self):
+        returnobj = self._data.popitem()
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+        return returnobj
+
+    def setdefault(self, key, default):
+        try:
+            return self[key]
+        except KeyError:
+            self[key] = default
+        return self[key]
+
+    def update(self, *args, **kwargs):
+        _args_ = [self._inputvalue(x) for x in args]
+        _kwargs_ = {k: self._inputvalue(v) for k, v in kwargs.items()}
+        self._data.update(*_args, **_kwargs)
+        if self._jsonfile.autosave:
+            self._jsonfile.save()
+
+    def values(self):
+        for key in self:
+            yield self[key]
 
 
 
-jsonfile = JSONFile
+jsonfile = JSONFileRoot
